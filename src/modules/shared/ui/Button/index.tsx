6b85c87cc88fb6070fa9f422582acf888804ed6e
Eduard Tsakhlo <edveinrin@gmail.com>
2025-04-26 20:43:17 +0300

    feat: add some basic `Button` styling

--- src/modules/shared/ui/Button/index.tsx
+++ src/modules/shared/ui/Button/index.tsx
@@ -1,8 +1,14 @@
 import classNames from "classnames";
 import { ButtonHTMLAttributes } from "react";
 
+import styles from "./styles.module.css";
+
 export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const Button = ({ className, ...restProps }: ButtonProps) => (
-  <button className={classNames(className)} type="button" {...restProps} />
+  <button
+    className={classNames(styles.root, className)}
+    type="button"
+    {...restProps}
+  />
 );
