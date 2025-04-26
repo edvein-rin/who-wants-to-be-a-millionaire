f896f355494bc4fd76c3f6d0ab4a167d0be4f7b8
Eduard Tsakhlo <edveinrin@gmail.com>
2025-04-26 19:03:02 +0300

    chore: remove useless test

--- src/modules/game/ui/Game/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { expect, it } from "vitest";
-import { render, screen } from "@testing-library/react";
-
-import { Game } from "./index";
-
-it("renders a placeholder", () => {
-  render(<Game />);
-  expect(screen.getByText("Here gonna be a game...")).toBeDefined();
-});
