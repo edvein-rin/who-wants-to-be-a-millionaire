06b9f1420a6918679a6b682e5c459f6703164026
Eduard Tsakhlo <edveinrin@gmail.com>
2025-04-27 20:25:23 +0300

    feat: style game over stage

--- src/modules/game/ui/GameOverStage/index.tsx
+++ src/modules/game/ui/GameOverStage/index.tsx
@@ -1,7 +1,9 @@
 import classNames from "classnames";
 
 import { Reward, formatReward } from "@/modules/reward";
-import { Button } from "@/modules/shared";
+import { Button, Image, Text } from "@/modules/shared";
+
+import styles from "./styles.module.css";
 
 export type GameOverStageProps = {
   reward: Reward;
@@ -14,8 +16,20 @@ export const GameOverStage = ({
   reward,
   onRestart,
 }: GameOverStageProps) => (
-  <main className={classNames(className)}>
-    <p>Score: {formatReward(reward)}</p>
-    <Button onClick={onRestart}>Try again</Button>
+  <main className={classNames(styles.root, className)}>
+    <div className={styles.imageWrapperWrapper}>
+      <div className={styles.imageWrapper}>
+        <Image alt="thumb-up" src="/images/thumb-up.svg" fill priority />
+      </div>
+    </div>
+    <div className={styles.cta}>
+      <div className={styles.score}>
+        <Text className={styles.scoreLabel}>Total score:</Text>
+        <Text className={styles.scoreValue} variant="h1">
+          {formatReward(reward)} earned
+        </Text>
+      </div>
+      <Button onClick={onRestart}>Try again</Button>
+    </div>
   </main>
 );
